90f921c35aa569d3053e492b5f2a9a70b29cf765
gere149 <gere.delatorre@gmail.com>
2026-02-16 11:51:37 +0100

    MovDog

--- Assets/!Practica/Scripts/Dog.cs
+++ Assets/!Practica/Scripts/Dog.cs
@@ -5,7 +5,30 @@ namespace Steerings
 
     public class Dog : SteeringBehaviour
     {
+        public float movSpeed = 5f;
+        public float rotationSpeed = 100f;
 
+        private Rigidbody2D rb;
+
+        void Start()
+        {
+            rb = GetComponent<Rigidbody2D>();
+        }
+
+        void Update()
+        {
+            rb.linearVelocity = transform.up * movSpeed;
+
+            float rotation = 0f;
+
+            if (Input.GetKey(KeyCode.A))
+                rotation = rotationSpeed;
+
+            if(Input.GetKey(KeyCode.D))
+                rotation = -rotationSpeed;
+
+            rb.MoveRotation(rb.rotation + rotation * Time.deltaTime);
+        }
         /*
         // remove comments for steerings that must be provided with a target 
         // remove whole block if no explicit target required
