240c6e029a65b017911f86a5d206618dc2f7ed6a
VhalroUni <avalerof@edu.tecnocampus.cat>
2026-02-20 11:17:58 +0100

    blackboard

---
+++ Assets/!Practica/Scripts/Blackboard.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Blackboard : MonoBehaviour
+{
+    //UI----------------------------------------------------------
+    public float timer = 120;
+
+    public float SheepAlive = 10;
+    public float SheepDeath = 10;
+    public float SheepSaved = 10;
+
+    //PERRO----------------------------------------------------------
+    public float gutDogRadius = 100;
+
+    //OVEJA----------------------------------------------------------
+    public float gutMetaRadius = 150;
+    public float gutSheepRadius = 150;
+
+    //LOVO----------------------------------------------------------
+    public float gutMadrigueraRadius = 150;
+    public float gutWolfaRadius = 150;
+
+    public float movSprint = 5f;
+}
